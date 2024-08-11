61355152c3d40cdea372080e3a3176305dc2a04a
Tabrez Ansari <tabrezsvce@gmail.com>
2024-08-11 18:41:18 +0530

    Update codedeploy_application_stop.sh

--- scripts/codedeploy_application_stop.sh
+++ scripts/codedeploy_application_stop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-#Stopping existing node servers
-echo "Stopping any existing node server"
-pkill node
+LOGFILE="/tmp/codedeploy_application_stop.log"
+echo "Stopping any existing node server" > $LOGFILE
+sudo pm2 stop all >> $LOGFILE 2>&1
+
+if [ $? -ne 0 ]; then
+  echo "Failed to stop node server" >> $LOGFILE
+  exit 1
+fi
+
+echo "Node server stopped successfully" >> $LOGFILE
+exit 0
